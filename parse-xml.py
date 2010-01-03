309f9875715836f71757f8d21cb5dd80e9b51ffe
Sean Kelly <github@katron.org>
2010-01-03 02:24:35 -0600

    Track atbat id

--- parse-xml.py
+++ parse-xml.py
@@ -25,14 +25,14 @@ def parse_game(game):
     atbat_fields = {'pitcher': 'pitcher', 'batter': 'batter', 'stand': 'batter_stand', 'p_throws': 'pitcher_throw', 'des': 'des', 'event': 'event', 'brief_event': 'brief_event'}
     atbat_sql = "INSERT INTO atbat (" + ",".join(atbat_fields[key] for key in sorted(atbat_fields.keys())) + ") VALUES (" + ",".join(map(lambda a: '?', atbat_fields.keys())) + ")"
     pitch_fields = {'spin_rate': 'spin_rate', 'break_angle': 'break_angle', 'pitch_type': 'pitch_type', 'ax': 'ax', 'ay': 'ay', 'y0': 'y0', 'az': 'az', 'end_speed': 'end_speed', 'spin_dir': 'spin_dir', 'start_speed': 'start_speed', 'pz': 'pz', 'px': 'px', 'type': 'type', 'sz_bot': 'sz_bot', 'pfx_z': 'pfx_z', 'vy0': 'vy0', 'pfx_x': 'pfx_x', 'break_length': 'break_length', 'x0': 'x0', 'z0': 'z0', 'break_y': 'break_y', 'sz_top': 'sz_top', 'type_confidence': 'type_confidence', 'y': 'y', 'x': 'x', 'vz0': 'vz0', 'sv_id': 'sv_id', 'vx0': 'vx0'}
-    pitch_sql = "INSERT INTO raw_pitch (" + ",".join(pitch_fields[key] for key in sorted(pitch_fields.keys())) + ",enhanced,balls,strikes) VALUES (" + ",".join(map(lambda a: '?', pitch_fields.keys())) + ",?,?,?)"
-    atbat_inserts = []
+    pitch_sql = "INSERT INTO raw_pitch (" + ",".join(pitch_fields[key] for key in sorted(pitch_fields.keys())) + ",atbat,enhanced,balls,strikes) VALUES (" + ",".join(map(lambda a: '?', pitch_fields.keys())) + ",?,?,?,?)"
     pitch_inserts = []
     for xml_file in xml_files:
         xml = etree.parse(os.path.join(game, xml_file))
         atbats = find_atbats(xml)
         for atbat in atbats:
-            atbat_inserts.append([atbat.get(key).strip() for key in sorted(atbat_fields.keys())])
+            cur = conn.execute(atbat_sql, [atbat.get(key).strip() for key in sorted(atbat_fields.keys())])
+            atbat_id = cur.lastrowid
             pitches = [i for i in atbat.getiterator('pitch')]
             balls, strikes = 0, 0
             for pitch in pitches:
@@ -44,12 +44,12 @@ def parse_game(game):
                 elif called == 'S' and (strikes < 2 or pitch.get('des') == 'Foul Tip'):
                     strikes += 1
 
+                insert.append(atbat_id)
                 insert.append(1 if pitch.get('pitch_type') else 0)
                 insert.append(balls)
                 insert.append(strikes)
                 pitch_inserts.append(insert)
 
-    conn.executemany(atbat_sql, atbat_inserts)
     conn.executemany(pitch_sql, pitch_inserts)
 
 
