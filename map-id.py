25fb0ababee95564a8e52be1e1dabf320ab30293
Sean Kelly <github@katron.org>
2010-01-03 02:05:33 -0600

    Add MLBAM id mappings
    This, right now, relies upon Tango's Player ID mappings release. In the
    future, use the players.xml in the game directory.

---
+++ map-id.py
@@ -0,0 +1,29 @@
+import csv, sqlite3
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-d", "--db", dest="db", help="SQLite database file")
+
+(options, args) = parser.parse_args()
+
+conn = sqlite3.connect(options.db)
+mlb_map = csv.reader(open(args[0]), delimiter="|")
+
+players = {}
+
+for row in conn.execute("SELECT mlbid,name FROM player"):
+    players[row[0]] = row[1]
+
+
+mappings = []
+
+for player in mlb_map:
+    # Make sure he's in MLB first.
+    # Then ensure he's not already in the database
+    if player[1] == 1 and player[0] not in players:
+        mappings.append([player[0], player[3]])
+
+conn.executemany("INSERT INTO player (mlbid, name) VALUES(?,?)", mappings)
+
+conn.commit()
+conn.close()
