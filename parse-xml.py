4c80f70a38b0006995eacb8aed8206e5001b0a97
Sean Kelly <github@katron.org>
2010-02-16 00:46:49 -0600

    Restructure code around parsing inning_N.xml

--- parse-xml.py
+++ parse-xml.py
@@ -17,9 +17,11 @@ if db_exists == False:
     conn.executescript(pfx.read())
 
 
-find_atbats = etree.XPath('/player/atbat')
+find_atbats = etree.XPath('/inning/*/atbat')
 def parse_game(game):
-    xml_files = fnmatch.filter(os.listdir(game), '*.xml')
+    # Skip inning_hit.xml. It is special and needs different processing.
+    xml_files = fnmatch.filter(os.listdir(game), 'inning_[!h]*.xml')
+
     # Keep these fields.
     # Right now this is all but one ('des'), but MLBAM has added fields before.
     atbat_fields = {'pitcher': 'pitcher', 'batter': 'batter', 'stand': 'batter_stand', 'p_throws': 'pitcher_throw', 'des': 'des', 'event': 'event', 'brief_event': 'brief_event'}
@@ -28,15 +30,19 @@ def parse_game(game):
     pitch_fields = {'spin_rate': 'spin_rate', 'break_angle': 'break_angle', 'pitch_type': 'pitch_type', 'ax': 'ax', 'ay': 'ay', 'y0': 'y0', 'az': 'az', 'end_speed': 'end_speed', 'spin_dir': 'spin_dir', 'start_speed': 'start_speed', 'pz': 'pz', 'px': 'px', 'type': 'type', 'sz_bot': 'sz_bot', 'pfx_z': 'pfx_z', 'vy0': 'vy0', 'pfx_x': 'pfx_x', 'break_length': 'break_length', 'x0': 'x0', 'z0': 'z0', 'break_y': 'break_y', 'sz_top': 'sz_top', 'type_confidence': 'type_confidence', 'y': 'y', 'x': 'x', 'vz0': 'vz0', 'sv_id': 'sv_id', 'vx0': 'vx0'}
     pitch_sql = "INSERT INTO raw_pitch (" + ",".join(pitch_fields[key] for key in sorted(pitch_fields.keys())) + ",atbat,enhanced,balls,strikes) VALUES (" + ",".join(map(lambda a: '?', pitch_fields.keys())) + ",?,?,?,?)"
     pitch_inserts = []
-    for xml_file in xml_files:
+
+    for inning in range(1, len(xml_files) + 1):
+        xml_file = 'inning' + str(inning) + '.xml'
         xml = etree.parse(os.path.join(game, xml_file))
         atbats = find_atbats(xml)
         for atbat in atbats:
+            # Try to match the atbat with entry in inning_hit.xml
+
             cur = conn.execute(atbat_sql, [atbat.get(key).strip() for key in sorted(atbat_fields.keys())])
             atbat_id = cur.lastrowid
-            pitches = [i for i in atbat.getiterator('pitch')]
+
             balls, strikes = 0, 0
-            for pitch in pitches:
+            for pitch in atbat.getiterator('pitch'):
                 insert = [pitch.get(key).strip() if pitch.get(key) else None for key in sorted(pitch_fields.keys())]
 
                 insert.append(atbat_id)
