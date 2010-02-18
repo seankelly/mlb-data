051663f42194efe192295a541b61af40f6ea26df
Sean Kelly <github@katron.org>
2010-02-18 15:47:20 -0600

    Spin off options stuff in pitchfx.py into __init__.py

---
+++ gameday/__init__.py
@@ -0,0 +1,91 @@
+__all__ = [ 'pitchfx' ]
+
+from datetime import date, datetime, timedelta
+from HTMLParser import HTMLParser
+from optparse import OptionParser
+import os, re
+
+def parse_date(date, default=date.today()):
+    if date:
+        try:
+            day = datetime.strptime(date, "%Y-%m-%d").date()
+        except ValueError:
+            print "Invalid date given:", date
+            raise
+    else:
+        day = default
+    return day
+
+
+class HTML(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.links = []
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'a':
+            self.links.append(dict(attrs))
+
+    def get_links(self, pattern):
+        link_match = re.compile(pattern)
+        links = []
+        for link in self.links:
+            if link_match.match(link['href']):
+                links.append(link['href'])
+
+        return links
+
+
+class Options(object):
+    def __init__(self):
+        self.start_day = date.today() - timedelta(1)
+        self.end_day = date.today() - timedelta(1)
+
+    def parse_options(self, parser=OptionParser()):
+        parser.add_option("-o", "--out", dest="outdir",
+                        help="XML saved in this directory")
+        parser.add_option("-s", "--start", dest="start_time", metavar="START",
+                        help="Start day")
+        parser.add_option("-e", "--end", dest="end_time", metavar="END",
+                        help="End day")
+        parser.add_option("-d", "--db", dest="db", metavar="DATABASE",
+                        help="Database to use")
+        parser.add_option("-D", "--driver", dest="driver", metavar="DRIVER",
+                        help="Database driver to use")
+        parser.add_option("-U", "--user", dest="user", metavar="USER",
+                        help="Database user to connect as")
+        parser.add_option("-P", "--password", dest="password", metavar="PASSWORD",
+                        help="Password for database")
+
+        (options, args) = parser.parse_args()
+        # Default the start day to yesterday
+        self.start_day = parse_date(options.start_time, self.start_day)
+        # Keep the default end day to be today
+        self.end_day = parse_date(options.end_time, self.end_day)
+
+        if self.start_day > self.end_day:
+            raise ValueError, "Starting day after ending day"
+
+        if options.outdir:
+            self.output_dir = os.path.abspath(options.outdir)
+            if os.path.exists(self.output_dir) == False:
+                os.makedirs(self.output_dir)
+        else:
+            raise ValueError, "Output directory not given"
+
+        if options.driver == 'sqlite' or options.driver == 'sqlite3':
+            import sqlite3
+            conn = sqlite3.connect(options.db)
+        elif options.driver == 'postgres' or options.driver == 'pygresql':
+            import pgdb
+            conn = pgdb.connect(database=options.db, user=options.user, password=options.password)
+        else:
+            raise ValueError, "No database driver specified"
+        self.conn = conn
+        return args
+
+    def each_day(self):
+        current_day = self.start_day
+        while current_day <= self.end_day:
+            yield current_day
+            current_day += timedelta(1)
--- gameday/pitchfx.py
+++ gameday/pitchfx.py
@@ -1,9 +1,3 @@
-from HTMLParser import HTMLParser
-from datetime import date, datetime, timedelta
-from optparse import OptionParser  
-from math import atan
-import os, re
-
 def counts():
     for balls in range(4):
         for strikes in range(3):
@@ -12,79 +6,6 @@ def counts():
 
 def flatten(L):
     return reduce(lambda x,y: x+y, L)
-
-def parse_date(date, default=date.today()):
-    if date:
-        try:
-            day = datetime.strptime(date, "%Y-%m-%d").date()
-        except ValueError:
-            print "Invalid date given:", date
-            raise
-    else:
-        day = default
-    return day
-
-
-class HTML(HTMLParser):
-    def __init__(self):
-        HTMLParser.__init__(self)
-        self.links = []
-
-    def handle_starttag(self, tag, attrs):
-        if tag == 'a':
-            self.links.append(dict(attrs))
-
-    def get_links(self, pattern):
-        link_match = re.compile(pattern)
-        links = []
-        for link in self.links:
-            if link_match.match(link['href']):
-                links.append(link['href'])
-
-        return links
-
-
-class PitchFX(object):
-    def __init__(self):
-        self.start_day = date.today() - timedelta(1)
-        self.end_day = date.today() - timedelta(1)
-
-    def parse_options(self, parser=OptionParser()):
-        parser.add_option("-o", "--out", dest="outdir",
-                        help="XML saved in this directory")
-        parser.add_option("-s", "--start", dest="start_time", metavar="START",
-                        help="Start day")
-        parser.add_option("-e", "--end", dest="end_time", metavar="END",
-                        help="End day")
-        parser.add_option("-d", "--db", dest="db", metavar="DATABASE",
-                        help="Database to use")
-        parser.add_option("-D", "--driver", dest="driver", metavar="DRIVER",
-                        help="Database driver to use")
-
-        (options, args) = parser.parse_args()
-        # Default the start day to yesterday
-        self.start_day = parse_date(options.start_time, self.start_day)
-        # Keep the default end day to be today
-        self.end_day = parse_date(options.end_time, self.end_day)
-
-        if self.start_day > self.end_day:
-            raise ValueError, "Starting day after ending day"
-
-        if options.outdir:
-            self.output_dir = os.path.abspath(options.outdir)
-            if os.path.exists(self.output_dir) == False:
-                os.makedirs(self.output_dir)
-        else:
-            raise ValueError, "Output directory not given"
-
-        return args
-
-
-    def each_day(self):
-        current_day = self.start_day
-        while current_day <= self.end_day:
-            yield current_day
-            current_day += timedelta(1)
 
 
 class Pitch(object):
