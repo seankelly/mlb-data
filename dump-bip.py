af902674fed1829e78f757bf682cced54bf68977
Sean Kelly <github@katron.org>
2010-02-28 01:38:14 -0600

    Also dump a parks.xml

--- dump-bip.py
+++ dump-bip.py
@@ -7,20 +7,33 @@ gd = gameday.Options()
 gd.parse_options()
 conn = gd.conn
 
-park_sql = "SELECT id, name, hp_x, hp_y, scale FROM park"
+park_sql = "SELECT id, name, hp_x, hp_y, scale FROM park ORDER BY id asc"
 bip_sql = "SELECT bip.x AS x, bip.y AS y, atbat.event AS event, bip.type AS type, p.name AS pitcher, b.name AS batter FROM bip JOIN park ON bip.park = park.id JOIN atbat ON bip.atbat = atbat.id LEFT JOIN player p ON p.mlbid = atbat.pitcher LEFT JOIN player b ON b.mlbid = atbat.batter where park.id = ?"
 bip_col = [ 'x', 'y', 'event', 'type', 'pitcher', 'batter' ]
 
+def dump_xml(filename, xml):
+    objectify.deannotate(xml)
+    etree.cleanup_namespaces(xml)
+    filename = os.path.join(gd.output_dir, filename)
+    xml_file = open(filename, "w")
+    xml_file.write(etree.tostring(xml))
+    xml_file.close()
+
 park = {}
 cur = conn.execute(park_sql)
+stadiums = objectify.Element("stadiums")
 for row in cur.fetchall():
     park[row[0]] = { 'name': row[1], 'hp_x': row[2], 'hp_y': row[3], 'scale': row[4] }
+    park_xml = objectify.Element("park")
+    for park_opt in park[row[0]].keys():
+        park_xml.set(park_opt, str(park[row[0]][park_opt]))
+    stadiums.append(park_xml)
+
+dump_xml("parks.xml", stadiums)
 
 for park_id in park.keys():
     cur = conn.execute(bip_sql, [park_id])
     park_xml = objectify.Element("park")
-    for park_opt in park[park_id].keys():
-        park_xml.set(park_opt, str(park[park_id][park_opt]))
     num_bip = 0
     for bip in cur.fetchall():
         num_bip += 1
@@ -30,9 +43,4 @@ for park_id in park.keys():
         park_xml.append(bip_xml)
     # No need to write empty files!
     if num_bip > 0:
-        objectify.deannotate(park_xml)
-        etree.cleanup_namespaces(park_xml)
-        filename = os.path.join(gd.output_dir, "park-" + str(park_id) + ".xml")
-        xml_file = open(filename, "w")
-        xml_file.write(etree.tostring(park_xml))
-        xml_file.close()
+        dump_xml("park-" + str(park_id) + ".xml", park_xml)
