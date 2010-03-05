9c2f02c95652610c62df7633661556e7bc4c6c9f
Sean Kelly <github@katron.org>
2010-03-05 14:57:55 -0600

    Include pitcher and batter throwing and batting side

--- dump-bip.py
+++ dump-bip.py
@@ -11,8 +11,8 @@ gd.parse_options()
 conn = gd.conn
 
 park_sql = "SELECT id, name, hp_x, hp_y, scale FROM park ORDER BY id asc"
-bip_sql = "SELECT bip.x AS x, bip.y AS y, atbat.event AS event, bip.type AS type, p.name AS pitcher, b.name AS batter FROM bip JOIN park ON bip.park = park.id JOIN atbat ON bip.atbat = atbat.id LEFT JOIN player p ON p.mlbid = atbat.pitcher LEFT JOIN player b ON b.mlbid = atbat.batter where park.id = ?"
-bip_col = [ 'x', 'y', 'event', 'type', 'pitcher', 'batter' ]
+bip_sql = "SELECT bip.x AS x, bip.y AS y, atbat.event AS event, bip.type AS type, p.name AS pitcher, atbat.pitcher_throw AS throw, b.name AS batter, atbat.batter_stand AS stand FROM bip JOIN park ON bip.park = park.id JOIN atbat ON bip.atbat = atbat.id LEFT JOIN player p ON p.mlbid = atbat.pitcher LEFT JOIN player b ON b.mlbid = atbat.batter where park.id = ?"
+bip_col = [ 'x', 'y', 'event', 'type', 'pitcher', 'throw', 'batter', 'stand' ]
 
 def dump_json(filename, obj):
     filename = os.path.join(gd.output_dir, filename)
@@ -34,7 +34,7 @@ for park_id in park.keys():
     cur = conn.execute(bip_sql, [park_id])
     bip_list = []
     for bip in cur.fetchall():
-        bip_list.append({ 'x':str(bip[0]), 'y':str(bip[1]), 'event':bip[2], 'type':bip[3], 'pitcher':bip[4], 'batter':bip[5] })
+        bip_list.append({ 'x': str(bip[0]), 'y': str(bip[1]), 'event': bip[2], 'type': bip[3], 'pitcher': bip[4], 'throw': bip[5], 'batter': bip[6], 'stand': bip[7]  })
     # No need to write empty files!
     if len(bip_list) > 0:
         dump_json("park-" + str(park_id) + ".json", bip_list)
