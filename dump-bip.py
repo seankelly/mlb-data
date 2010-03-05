798b84df21e830d0377531f9e47335cf4cc450ef
Sean Kelly <github@katron.org>
2010-03-05 00:39:51 -0600

    Dump JSON instead of XML

--- dump-bip.py
+++ dump-bip.py
@@ -1,7 +1,10 @@
 # Dump XML files containing BIP data for each park.
 
 import gameday, os
-from lxml import objectify, etree
+try:
+    import json
+except ImportError:
+    import simplejson as json
 
 gd = gameday.Options()
 gd.parse_options()
@@ -11,36 +14,27 @@ park_sql = "SELECT id, name, hp_x, hp_y, scale FROM park ORDER BY id asc"
 bip_sql = "SELECT bip.x AS x, bip.y AS y, atbat.event AS event, bip.type AS type, p.name AS pitcher, b.name AS batter FROM bip JOIN park ON bip.park = park.id JOIN atbat ON bip.atbat = atbat.id LEFT JOIN player p ON p.mlbid = atbat.pitcher LEFT JOIN player b ON b.mlbid = atbat.batter where park.id = ?"
 bip_col = [ 'x', 'y', 'event', 'type', 'pitcher', 'batter' ]
 
-def dump_xml(filename, xml):
-    objectify.deannotate(xml)
-    etree.cleanup_namespaces(xml)
+def dump_json(filename, obj):
     filename = os.path.join(gd.output_dir, filename)
-    xml_file = open(filename, "w")
-    xml_file.write(etree.tostring(xml))
-    xml_file.close()
+    json_file = open(filename, "w")
+    json.dump(obj, json_file, separators=(',',':'))
+    json_file.close()
 
 park = {}
 cur = conn.execute(park_sql)
-stadiums = objectify.Element("stadiums")
+stadiums = []
 for row in cur.fetchall():
-    park[row[0]] = { 'id': row[0], 'name': row[1], 'hp_x': row[2], 'hp_y': row[3], 'scale': row[4] }
-    park_xml = objectify.Element("park")
-    for park_opt in park[row[0]].keys():
-        park_xml.set(park_opt, str(park[row[0]][park_opt]))
-    stadiums.append(park_xml)
+    park[row[0]] = { 'id': row[0], 'name': row[1], 'hp_x': str(row[2]), 'hp_y': str(row[3]), 'scale': str(row[4]) }
+    park_json = park[row[0]]
+    stadiums.append(park_json)
 
-dump_xml("parks.xml", stadiums)
+dump_json("parks.json", stadiums)
 
 for park_id in park.keys():
     cur = conn.execute(bip_sql, [park_id])
-    park_xml = objectify.Element("park")
-    num_bip = 0
+    park_json = []
     for bip in cur.fetchall():
-        num_bip += 1
-        bip_xml = objectify.Element("bip")
-        for i, col in enumerate(bip_col):
-            bip_xml.set(col, str(bip[i]))
-        park_xml.append(bip_xml)
+        park_json.append({ 'x':str(bip[0]), 'y':str(bip[1]), 'event':bip[2], 'type':bip[3], 'pitcher':bip[4], 'batter':bip[5] })
     # No need to write empty files!
-    if num_bip > 0:
-        dump_xml("park-" + str(park_id) + ".xml", park_xml)
+    if len(park_json) > 0:
+        dump_json("park-" + str(park_id) + ".json", park_json)
