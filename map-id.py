466000f6f5a35dfdcb804ae5702c66443f9796e8
Sean Kelly <github@katron.org>
2010-03-11 17:44:31 -0600

    Convert map-id.py to sqlalchemy

--- map-id.py
+++ map-id.py
@@ -1,28 +1,29 @@
 import csv, gameday
 from optparse import OptionParser
+from sqlalchemy.sql import select
 
 parser = OptionParser()
 parser.add_option("-f", "--file", dest="player_map", help="MLBAM ID player mapping")
 
 gd = gameday.Options()
 (options, args) = gd.parse_options(parser)
+gd.init_db()
 
 mlb_map = csv.reader(open(options.player_map), delimiter="|")
+player_table = gd.meta.tables['player']
 
+s = select([player_table])
 players = {}
-cur = gd.conn.cursor()
-
-cur.execute("SELECT mlbid,name FROM player")
-for row in cur.fetchall():
-    players[row[0]] = row[1]
+for row in gd.conn.execute(s):
+    players[row['name']] = row['mlbid']
 
 mappings = []
-
 for player in mlb_map:
     # Make sure he's in MLB first.
     # Then ensure he's not already in the database
     if int(player[1]) == 1 and player[0] not in players:
-        mappings.append([player[0], player[3]])
+        mappings.append({ 'mlbid': player[0], 'name': player[3] })
 
-cur.executemany("INSERT INTO player (mlbid, name) VALUES(?,?)", mappings)
-cur.commit()
+trans = gd.conn.begin()
+gd.conn.execute(player_table.insert(), mappings)
+trans.commit()
