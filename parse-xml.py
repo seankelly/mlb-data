96502b143d1b42fc94658e7606cef92954ef60f9
Sean Kelly <github@katron.org>
2011-04-12 21:26:43 -0500

    Durr, inserted_primary_key is a list, not a method to return a list

--- parse-xml.py
+++ parse-xml.py
@@ -46,8 +46,7 @@ def parse_game_xml(game_dir, day):
         elif el.tag == 'stadium':
             game['park'] = el.get('id')
     res = gd.conn.execute(game_ins, game)
-    ids = res.inserted_primary_key()
-    game['id'] = ids[0]
+    game['id'] = res.inserted_primary_key[0]
     return game
 
 
@@ -83,8 +82,7 @@ def parse_game(game_dir, day):
                 if ab_data[key]:
                     ab_data[key] = ab_data[key].strip()
             res = gd.conn.execute(ab_ins, ab_data)
-            ids = res.inserted_primary_key()
-            ab_id = ids[0]
+            ab_id = res.inserted_primary_key[0]
 
             # Try to match the atbat with entry in inning_hit.xml
             idx = len(bip)-1
