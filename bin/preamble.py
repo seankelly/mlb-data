d867e92588ac578d8e51d29bd4dab9316bb6b861
Sean Kelly <github@katron.org>
2012-04-13 19:34:40 -0500

    Add a preamble import so I don't need to use PYTHONPATH

---
+++ bin/preamble.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+root = os.path.abspath(sys.argv[0])
+while os.path.dirname(root) != root:
+    init_path = os.path.join(root, 'mlbam', '__init__.py')
+    if os.path.exists(init_path):
+        sys.path.insert(0, root)
+        break
+    root = os.path.dirname(root)
