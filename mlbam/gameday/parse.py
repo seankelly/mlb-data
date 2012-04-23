5cd6d22ea10c9c39ecbe4909fc65ad5411212765
Sean Kelly <github@katron.org>
2012-04-22 22:34:33 -0500

    Rework a lot of code
    This is a WIP commit. I guarantee it doesn't work, since I'm sure I
    missed quite a few variable renames. I think this should be mostly
    working soon.

--- mlbam/gameday/parse.py
+++ mlbam/gameday/parse.py
@@ -39,25 +39,77 @@ class GamedayParser():
                 'last': player.get('last'),
             }
 
+    def _parse_inning_hit_xml(self):
+        xml_file = etree.parse(os.path.join(self.directory, 'inning_hit.xml'))
+        BIP = []
+        for bip in self.find_bip(xml_file)
+            x = int(float(bip.get('x')) * 249/250)
+            y = int(float(bip.get('y')) * 249/250)
+            # Skip the 'des' field, since it will be in the atbat XML.
+            BIP.append({'x': x, 'y': y, 'park': self.game['park'], 'type':
+                        bip.get('type')}]
+        self.game['bip'] = BIP
+
+    def _parse_pitches(self, atbat, ab_data):
+        pitch_fields = set(['spin_rate', 'break_angle', 'pitch_type', 'ax',
+            'ay', 'y0', 'az', 'end_speed', 'spin_dir', 'start_speed', 'pz',
+            'px', 'type', 'sz_bot', 'pfx_z', 'vy0', 'pfx_x', 'break_length',
+            'x0', 'z0', 'break_y', 'sz_top', 'type_confidence', 'y', 'x',
+            'vz0', 'sv_id', 'vx0', 'des'])
+        balls, strikes = 0, 0
+        sequence = 1
+        for pitch in atbat.getiterator('pitch'):
+            enhanced = True if pitch.get('pitch_type') else False
+            pitch_data = {'enhanced': enhanced, 'balls': balls, 'strikes':
+                    strikes, 'sequence': sequence, 'batter': ab_data['batter'],
+                    'pitcher': ab_data['pitcher']}
+            for key in pitch_fields:
+                pitch_data[key] = pitch.get(key)
+                if pitch_data[key]:
+                    pitch_data[key] = pitch_data[key].strip()
+            sequence += 1
+            # The count is the balls and strikes for when the pitch was
+            # delivered, rather than the resulting count.
+            called = pitch.get('type')
+            des = pitch.get('des')
+            if called == 'B':
+                balls += 1
+            elif (called == 'S' and (strikes < 2 or (des != 'Foul' and des !=
+                  'Foul (Runner Going)'))):
+                strikes += 1
+
+    def _parse_inning_xml(self):
+        # Keep these fields.
+        # 'atbat' is a misnomer, since these are actually plate appearances.
+        # However, since the XML files use the term atbat, use that.
+        ab_fields = {'pitcher': 'pitcher', 'batter': 'batter', 'stand':
+                'batter_stand', 'p_throws': 'pitcher_throw', 'des': 'des',
+                'event': 'event'}
+        atbats = []
+        # Skip inning_hit.xml. It is special and needs different processing.
+        xml_files = fnmatch.filter(os.listdir(self.directory), 'inning_[!h]*.xml')
+        for inning in range(1, len(xml_files) + 1):
+            xml_file = os.path.join(self.directory, 'inning_' + str(inning) + '.xml')
+            inning_ab = self.find_atbats(etree.parse(xml_file)))
+            for atbat in inning_ab:
+                ab_data = {'inning': inning}
+                for field in ab_fields:
+                    key = ab_fields[field]
+                    ab_data[key] = atbat.get(field)
+                    if ab_data[key]:
+                        ab_data[key] = ab_data[key].strip()
+                ab_data['pitches'] = self._parse_pitches(atbat, ab_data)
+                atbats.append(ab_data)
+        self.game['atbats'] = atbats
+
     def _parse(self):
         self._parse_game_xml()
         self._parse_players_xml()
-        add_players_xml(game_dir)
-        # Skip inning_hit.xml. It is special and needs different processing.
-        xml_files = fnmatch.filter(os.listdir(game_dir), 'inning_[!h]*.xml')
-        bip = find_bip(etree.parse(os.path.join(game_dir, 'inning_hit.xml')))
-        # Reverse it because python 2.5 doesn't have popleft()
-        bip.reverse()
-
-        # Keep these fields.
-        # Right now this is all but one ('brief_event'), but MLBAM has added fields before.
-        ab_fields = {'pitcher': 'pitcher', 'batter': 'batter', 'stand': 'batter_stand', 'p_throws': 'pitcher_throw', 'des': 'des', 'event': 'event'}
-        # Add in bip_x/y columns
-        ab_ins = gd.meta.tables['atbat'].insert()
-        bip_ins = gd.meta.tables['bip'].insert()
-        pitch_fields = {'spin_rate': 'spin_rate', 'break_angle': 'break_angle', 'pitch_type': 'pitch_type', 'ax': 'ax', 'ay': 'ay', 'y0': 'y0', 'az': 'az', 'end_speed': 'end_speed', 'spin_dir': 'spin_dir', 'start_speed': 'start_speed', 'pz': 'pz', 'px': 'px', 'type': 'type', 'sz_bot': 'sz_bot', 'pfx_z': 'pfx_z', 'vy0': 'vy0', 'pfx_x': 'pfx_x', 'break_length': 'break_length', 'x0': 'x0', 'z0': 'z0', 'break_y': 'break_y', 'sz_top': 'sz_top', 'type_confidence': 'type_confidence', 'y': 'y', 'x': 'x', 'vz0': 'vz0', 'sv_id': 'sv_id', 'vx0': 'vx0', 'des': 'des'}
-        pitch_ins = gd.meta.tables['raw_pitch'].insert()
-        pitch_inserts = []
+        # Parse the inning_hit.xml first to make it easier to match each bip
+        # with data in the inning XML files.
+        self._parse_inning_hit_xml()
+        self._parse_inning_xml()
+        return
 
         for inning in range(1, len(xml_files) + 1):
             xml_file = 'inning_' + str(inning) + '.xml'
@@ -84,63 +136,9 @@ class GamedayParser():
                     bip.pop()
                     idx = len(bip)-1
 
-                balls, strikes = 0, 0
-                sequence = 1
-                for pitch in atbat.getiterator('pitch'):
-                    enhanced = True if pitch.get('pitch_type') else False
-                    pitch_data = { 'atbat': ab_id, 'enhanced': enhanced, 'balls': balls, 'strikes': strikes, 'sequence': sequence, 'game': game['id'], 'batter': ab_data['batter'], 'pitcher': ab_data['pitcher'] }
-                    for key in pitch_fields:
-                        pitch_data[key] = pitch.get(key)
-                        if pitch_data[key]:
-                            pitch_data[key] = pitch_data[key].strip()
-                    pitch_inserts.append(pitch_data)
-
-                    sequence += 1
-                    # Calculate the count
-                    # ..after the pitch!
-                    called = pitch.get('type')
-                    des    = pitch.get('des')
-                    if called == 'B':
-                        balls += 1
-                    elif called == 'S' and (strikes < 2 or (des != 'Foul' and des != 'Foul (Runner Going)')):
-                        strikes += 1
-
 
 def load_players():
     player_table = gd.meta.tables['mlbam_player']
     select_ids = select([player_table.c.mlbamid], player_table.c.mlbamid != None)
     for row in gd.conn.execute(select_ids):
         mlbamids.add(row['mlbamid'])
-
-find_players = etree.XPath('/game/team/player')
-def add_players_xml(game_dir):
-    players_xml = os.path.join(game_dir, 'players.xml')
-    if not os.path.exists(players_xml):
-        return
-    add_player = gd.meta.tables['mlbam_player'].insert()
-    ins = []
-    for player in find_players(etree.parse(players_xml)):
-        mlbid = int(player.get('id'))
-        if mlbid not in mlbamids:
-            ins.append({ 'mlbamid': mlbid, 'namefirst': player.get('first'), 'namelast': player.get('last') })
-            mlbamids.add(mlbid)
-    if (len(ins) > 0):
-        gd.conn.execute(add_player, ins)
-
-
-game_children = etree.XPath('/game/*')
-def parse_game_xml(game_dir, day):
-    game_ins = gd.meta.tables['game'].insert()
-    game = { 'day': str(day) }
-    for el in game_children(etree.parse(os.path.join(game_dir, 'game.xml'))):
-        if el.tag == 'team':
-            game[el.get('type')] = el.get('id')
-        elif el.tag == 'stadium':
-            game['park'] = el.get('id')
-    res = gd.conn.execute(game_ins, game)
-    game['id'] = res.inserted_primary_key[0]
-    return game
-
-
-find_atbats = etree.XPath('/inning/*/atbat')
-find_bip = etree.XPath('/hitchart/hip')
