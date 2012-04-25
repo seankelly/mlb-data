ca32da7c2797f0a272997d68b4713fdc0f5481f4
Sean Kelly <github@katron.org>
2012-04-24 22:08:24 -0500

    Set default start and end dates to yesterday
    This matches previous behavior.

--- mlb/util.py
+++ mlb/util.py
@@ -1,11 +1,15 @@
 import argparse
+import datetime
 
 def commandline_args(desc):
+    yesterday = datetime.date.today() - datetime.timedelta(1)
     parser = argparse.ArgumentParser(description=desc)
     parser.add_argument('-o', '--output_dir', metavar='DIR',
                         help='Output directory')
-    parser.add_argument('-s', '--start', metavar='DATE', help='Start day')
-    parser.add_argument('-e', '--end', metavar='DATE', help='End day')
+    parser.add_argument('-s', '--start', metavar='DATE',
+                        default=yesterday, help='Start day')
+    parser.add_argument('-e', '--end', metavar='DATE',
+                        default=yesterday, help='End day')
     parser.add_argument('-l', '--leagues', action='append',
                         help='Fetch these leagues')
     args = parser.parse_args()
@@ -15,4 +19,4 @@ def each_day(start_day, end_day):
     current_day = start_day
     while current_day <= end_day:
         yield current_day
-        current_day += timedelta(1)
+        current_day += datetime.timedelta(1)
