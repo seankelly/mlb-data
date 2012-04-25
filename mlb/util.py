45e6e517dfc01d6b33fcc4627babf69c45cbc0ae
Sean Kelly <github@katron.org>
2012-04-24 21:53:16 -0500

    Add each_day function

--- mlb/util.py
+++ mlb/util.py
@@ -10,3 +10,9 @@ def commandline_args(desc):
                         help='Fetch these leagues')
     args = parser.parse_args()
     return args
+
+def each_day(start_day, end_day):
+    current_day = start_day
+    while current_day <= end_day:
+        yield current_day
+        current_day += timedelta(1)
