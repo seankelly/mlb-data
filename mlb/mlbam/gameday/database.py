1f5ce007034d41edca4f71cca780c238772b2c0c
Sean Kelly <github@katron.org>
2012-04-26 22:49:10 -0500

    Add some comments
    The command line interfaces should probably be moved to a different
    namespace. This way, all of the bin/ scripts just call those directly
    and they handle the command line junk. Then, the modules in mlb.mlbam
    are internal only.

--- mlb/mlbam/gameday/database.py
+++ mlb/mlbam/gameday/database.py
@@ -5,5 +5,8 @@ Add parsed Gameday data to the database.
 from sqlalchemy import MetaData, create_engine
 from sqlalchemy.sql import select
 
+# No command line interface because it requires parsed Gameday data. There
+# currently isn't a way to save the parsed data to disk, so it would be wasted
+# work to add a command line interface.
 def add_to_db(options, games):
     pass
