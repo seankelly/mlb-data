c7e7c3bc83c91d8b76839bb182adfeca1f53bd12
Sean Kelly <github@katron.org>
2012-04-27 23:42:55 -0500

    Add stub functions

--- mlb/mlbam/gameday/database.py
+++ mlb/mlbam/gameday/database.py
@@ -5,15 +5,27 @@ Add parsed Gameday data to the database.
 from sqlalchemy import MetaData, create_engine
 from sqlalchemy.sql import select
 
-def load_players():
-    player_table = gd.meta.tables['mlbam_player']
-    select_ids = select([player_table.c.mlbamid], player_table.c.mlbamid != None)
-    for row in gd.conn.execute(select_ids):
-        mlbamids.add(row['mlbamid'])
-
 # No command line interface because it requires parsed Gameday data. There
 # currently isn't a way to save the parsed data to disk, so it would be wasted
 # work to add a command line interface.
 def add_to_db(options, games):
     if 'database' not in options or not options['database']:
         raise ValueError("No database specified.")
+
+def insert_games():
+    pass
+
+def connect_db():
+    pass
+
+def load_players(conn):
+    mlbamids = set()
+    player_table = gd.meta.tables['mlbam_player']
+    select_ids = select([player_table.c.mlbamid],
+                        player_table.c.mlbamid != None)
+    for row in gd.conn.execute(select_ids):
+        mlbamids.add(row['mlbamid'])
+    return mlbamids
+
+def insert_game(conn, game):
+    pass
