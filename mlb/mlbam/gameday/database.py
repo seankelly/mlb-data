d18874789d27c3e93ddb94538c6b243bdeb3902c
Sean Kelly <github@katron.org>
2012-04-27 22:40:47 -0500

    Move load_players() to parse-game-xml
    The parse_day function isn't needed though, so delete it.

--- mlb/mlbam/gameday/database.py
+++ mlb/mlbam/gameday/database.py
@@ -5,6 +5,12 @@ Add parsed Gameday data to the database.
 from sqlalchemy import MetaData, create_engine
 from sqlalchemy.sql import select
 
+def load_players():
+    player_table = gd.meta.tables['mlbam_player']
+    select_ids = select([player_table.c.mlbamid], player_table.c.mlbamid != None)
+    for row in gd.conn.execute(select_ids):
+        mlbamids.add(row['mlbamid'])
+
 # No command line interface because it requires parsed Gameday data. There
 # currently isn't a way to save the parsed data to disk, so it would be wasted
 # work to add a command line interface.
