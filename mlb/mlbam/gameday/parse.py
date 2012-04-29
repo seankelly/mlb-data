97fa7eb4e271e375a476db5abe4a7af19b2beb48
Sean Kelly <github@katron.org>
2012-04-29 00:50:30 -0500

    Oops, taking the length of the wrong string

--- mlb/mlbam/gameday/parse.py
+++ mlb/mlbam/gameday/parse.py
@@ -47,7 +47,7 @@ class GamedayParser():
                     # This is to ensure the name is the full, unabbreviated
                     # team name. For instance, the Diamondbacks are abbreviated
                     # as 'D-backs' when I want the full name.
-                    team_name = fullname[len(fullname)].strip()
+                    team_name = fullname[len(location)].strip()
                 else:
                     # Just in case.
                     team_name = el.get('name_brief').
