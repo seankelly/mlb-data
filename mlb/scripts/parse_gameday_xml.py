a3444e599d404eddcabdff30aa48f9171003ae51
Sean Kelly <github@katron.org>
2012-06-04 18:40:08 -0500

    Fix the imports
    I think I completely forgot how python importing worked, hence all the
    trouble. Simplified things a little bit and now it should work.

--- mlb/scripts/parse_gameday_xml.py
+++ mlb/scripts/parse_gameday_xml.py
@@ -1,7 +1,8 @@
 import os.path
-import mlb.mlbam.gameday as gameday
+import mlb.mlbam.gameday.database as db
 from mlb.database import connect_db
 from mlb.util import commandline_args, each_day
+from mlb.mlbam.gameday.parse import parse_day
 
 def run():
     parsed_games = []
@@ -10,14 +11,14 @@ def run():
     for league in args['leagues']:
         dir = os.path.join(args['output_dir'], league)
         for day in each_day(args['start'], args['end']):
-            parsed_games.extend(gameday.parse.parse_day(dir, day))
+            parsed_games.extend(parse_day(dir, day))
         # Since I'm running this on a memory constrained system, flush the
         # games to a database periodically. 300 games is a little over three
         # weeks during the middle of the season. The system can handle a full
         # month, but I get the feeling it's swapping when it hits 400 games.
         if len(parsed_games) > 300:
-            gameday.database.insert_games(conn, meta, parsed_games)
+            db.insert_games(conn, meta, parsed_games)
             parsed_games = []
     # And insert any remaining games.
     if parsed_games:
-        gameday.database.insert_games(conn, meta, parsed_games)
+        db.insert_games(conn, meta, parsed_games)
