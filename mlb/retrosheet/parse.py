2577253625e870798f0b4da53934bfd9d862eb17
Sean Kelly <github@katron.org>
2012-07-26 22:37:20 -0500

    Store the game info by year
    Save the year for all the games so they can be grouped together.

--- mlb/retrosheet/parse.py
+++ mlb/retrosheet/parse.py
@@ -14,20 +14,23 @@ def game_info(event_files):
     """
     # Since Chadwick requires the roster files to be in the current directory,
     # change directory to the event files to parse before parsing.
-    info = []
+    info = {}
     start_cwd = os.getcwd()
     # Need to get the year to tell cwgame what year roster to look up.
     get_year = re.compile('^(\d+)')
     games = 0
     for f in event_files:
         file_dir, base_file = os.path.split(f)
-        year = get_year.match(base_file)
+        Y = get_year.match(base_file)
+        year = Y.group(1)
+        if year not in info:
+            info[year] = []
         os.chdir(file_dir)
-        cwgame = ['cwgame', '-y', year.group(1), base_file]
+        cwgame = ['cwgame', '-y', year, base_file]
         p = subprocess.Popen(cwgame, stdout=subprocess.PIPE,
                                      stderr=subprocess.PIPE)
         # Ignore stderr. I don't want it echoed or merged with stdout.
         for game in p.stdout.readlines():
-            info.append(game)
+            info[year].append(game)
         os.chdir(start_cwd)
     return info
