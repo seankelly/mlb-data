b681f7497a6b6ad6a1661b7c440fe185acf22af1
Sean Kelly <github@katron.org>
2012-07-31 22:47:38 -0500

    Init the HDF5 file
    Data is parsed and the HDF5 file is initialized, so now time to
    experiment inserting the data.

--- mlb/retrosheet/parse.py
+++ mlb/retrosheet/parse.py
@@ -5,9 +5,11 @@ http://chadwick.sourceforge.net
 """
 
 import csv
+import h5py
 import os
 import re
 import subprocess
+from ..hdf5 import init_hdf5
 
 def game_info(event_files):
     """
@@ -37,6 +39,9 @@ def game_info(event_files):
     return info
 
 def parse_game_info(hdf5_file, event_files):
+    h5_file = h5py.File(hdf5_file)
+    init_hdf5(h5_file)
     csv_info = game_info(event_files)
     for year in csv_info:
         info = csv.reader(csv_info[year])
+    h5_file.close()
