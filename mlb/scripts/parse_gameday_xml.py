2f0c707e61577286eda7a56f727919ddbb15e003
Sean Kelly <github@katron.org>
2012-10-14 14:02:40 -0500

    Missed another commandline_args returning a tuple
    Properly unpack the tuple. Also effectively renamed 'args' to 'options'.

--- mlb/scripts/parse_gameday_xml.py
+++ mlb/scripts/parse_gameday_xml.py
@@ -6,11 +6,11 @@ from mlb.mlbam.gameday.parse import parse_day
 
 def run():
     parsed_games = []
-    args = commandline_args('Parse Gameday XML files')
-    conn, meta = connect_db(args['database'])
-    for league in args['leagues']:
-        dir = os.path.join(args['output_dir'], league)
-        for day in each_day(args['start'], args['end']):
+    options, args = commandline_args('Parse Gameday XML files')
+    conn, meta = connect_db(options['database'])
+    for league in options['leagues']:
+        dir = os.path.join(options['output_dir'], league)
+        for day in each_day(options['start'], options['end']):
             parsed_games.extend(parse_day(dir, day))
         # Since I'm running this on a memory constrained system, flush the
         # games to a database periodically. 300 games is a little over three
