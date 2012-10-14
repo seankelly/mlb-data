5c69821add33f0a635b04d0699f3b92937d1c201
Sean Kelly <github@katron.org>
2012-10-14 14:49:04 -0500

    Oops, wasn't actually inserting until everything was parsed
    Got confused about the program flow. Now it should insert after every
    game. I think it will go quickly enough. I wonder if I had the program
    flow wrong earlier as well, when trying to bulk insert many games. It
    seemed to be able to do 10-15 days worth of games (150-225 games)
    without memory problems.
    
    Just checked and yes, I had the if statement outside the for loop. No
    wonder I was having problems!

--- mlb/scripts/parse_gameday_xml.py
+++ mlb/scripts/parse_gameday_xml.py
@@ -5,12 +5,10 @@ from mlb.util import commandline_args, each_day
 from mlb.mlbam.gameday.parse import parse_day
 
 def run():
-    parsed_games = []
     options, args = commandline_args('Parse Gameday XML files')
     conn, meta = connect_db(options['database'])
     for league in options['leagues']:
         dir = os.path.join(options['output_dir'], league)
         for day in each_day(options['start'], options['end']):
-            parsed_games.extend(parse_day(dir, day))
-        db.insert_games(conn, meta, parsed_games)
-        parsed_games = []
+            parsed_games = parse_day(dir, day)
+            db.insert_games(conn, meta, parsed_games)
