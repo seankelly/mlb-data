fbe2689b700fa1281a63d47b4a4f7a66bfa9d23a
Sean Kelly <github@katron.org>
2012-10-14 14:30:32 -0500

    Just flush after each day
    I pushed the limit down to 50 and still the process was killed. I may
    have to add each game after its processed to keep the memory load low.

--- mlb/scripts/parse_gameday_xml.py
+++ mlb/scripts/parse_gameday_xml.py
@@ -12,12 +12,5 @@ def run():
         dir = os.path.join(options['output_dir'], league)
         for day in each_day(options['start'], options['end']):
             parsed_games.extend(parse_day(dir, day))
-        # Since I'm running this on a memory constrained system, flush the
-        # games to a database periodically. Currently trying for every 150
-        # games, since 300 was about twice the memory I wanted to use.
-        if len(parsed_games) > 150:
-            db.insert_games(conn, meta, parsed_games)
-            parsed_games = []
-    # And insert any remaining games.
-    if parsed_games:
         db.insert_games(conn, meta, parsed_games)
+        parsed_games = []
