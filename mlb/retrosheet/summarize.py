4c434fc1bf0894ab9ab0427cb97e594e6d099c2f
Sean Kelly <code@katron.org>
2013-02-16 16:29:44 -0600

    Add script summarize Retrosheet stats
    No summarizing, yet.

--- mlb/retrosheet/summarize.py
+++ mlb/retrosheet/summarize.py
@@ -1,3 +1,6 @@
 """
 Summarize Retrosheet stats into game and season summaries, and splits.
 """
+
+def summarize_stats(hdf5_file):
+    pass
---
+++ mlb/scripts/summarize_retrosheet.py
@@ -0,0 +1,6 @@
+from mlb.retrosheet.summarize import summarize_stats
+from ..util import commandline_args
+
+def run():
+    args, extra_args = commandline_args('Summarize Retrosheet data')
+    summarize_stats(args['file'])
