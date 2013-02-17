ff84e80a40d74e87e3fdf0a01f3d33b0a0c29cce
Sean Kelly <code@katron.org>
2013-02-16 22:40:51 -0600

    Don't need to parse the start and end dates
    Forgot I already did that. Otherwise, I was about to do it again.

--- mlb/retrosheet/summarize.py
+++ mlb/retrosheet/summarize.py
@@ -6,10 +6,8 @@ from datetime import datetime
 
 def summarize_stats(args):
     hdf5_file = args['file']
-    start_time = args['start']
-    end_time = args['end']
-    start = datetime.strptime(start_time, '%Y-%m-%d')
-    end = datetime.strptime(end_time, '%Y-%m-%d')
+    start = args['start']
+    end = args['end']
     h5_file = h5py.File(hdf5_file)
     summarize_games(h5_file, start, end)
     h5_file.close()
