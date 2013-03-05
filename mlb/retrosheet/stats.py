f967b7e0cf2feb2ca4d89dc120e7f101a1ca0319
Sean Kelly <code@katron.org>
2013-03-04 18:49:37 -0600

    Add fielding stats
    Start splitting up defense stats into pitching and fielding. The
    fielding stats will be applied for each position in a year.

--- mlb/retrosheet/stats.py
+++ mlb/retrosheet/stats.py
@@ -8,6 +8,7 @@ import numpy as np
 def get_stats_mapping():
     offense_stats = ['PA', 'AB', 'R', '1B', '2B', '3B', 'HR', 'ROE', 'RBI', 'K', 'BB', 'IBB', 'HBP', 'O', 'SF', 'SH', 'SB', 'CS', 'PO',]
     defense_stats = ['O', 'R', 'ER', 'K', 'BB', 'IBB', 'HBP', 'SB', 'CS', 'PO', 'WP', 'PB', 'E', '1B', '2B', '3B', 'HR']
+    fielding_stats = ['Year', 'Pos', 'Ch', 'PO', 'A', 'E', 'DP', 'SB', 'CS', 'WP', 'PB', 'Pickoff',]
     offense_map = {}
     defense_map = {}
     for index, stat in enumerate(offense_stats):
