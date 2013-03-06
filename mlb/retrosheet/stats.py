a0ace6ce9fb45ace39c1132c0477271a52e97d85
Sean Kelly <code@katron.org>
2013-03-05 18:51:13 -0600

    Allow balks to be counted

--- mlb/retrosheet/stats.py
+++ mlb/retrosheet/stats.py
@@ -8,7 +8,7 @@ import numpy as np
 def get_stats_mapping():
     stats = {
         'offense': ['PA', 'AB', 'R', '1B', '2B', '3B', 'HR', 'ROE', 'RBI', 'K', 'BB', 'IBB', 'HBP', 'O', 'SF', 'SH', 'SB', 'CS', 'PO',],
-        'pitching': ['G', 'GS', 'GF', 'CG', 'SHO', 'W', 'L', 'S', 'O', 'R', 'ER', 'K', 'BB', 'IBB', 'HBP', 'SB', 'CS', 'WP', '1B', '2B', '3B', 'HR', 'GDP', 'ROE',],
+        'pitching': ['G', 'GS', 'GF', 'CG', 'SHO', 'W', 'L', 'S', 'O', 'R', 'ER', 'K', 'BB', 'IBB', 'HBP', 'BK', 'SB', 'CS', 'WP', '1B', '2B', '3B', 'HR', 'GDP', 'ROE',],
         'fielding': ['Pos', 'Ch', 'PO', 'A', 'E', 'DP', 'SB', 'CS', 'WP', 'PB', 'Pickoff',],
     }
     stat_map = {}
