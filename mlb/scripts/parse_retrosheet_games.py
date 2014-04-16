49f158dfa88e30f538d1d8ab8042036ff76acbe9
Sean Kelly <code@katron.org>
2014-04-16 07:08:52 -0500

    Simplistic conversion to using renamed Chadwick module
    This doesn't actually do anything, but the previous version didn't do
    anything useful with the data from Chadwick.

--- mlb/scripts/parse_retrosheet_games.py
+++ mlb/scripts/parse_retrosheet_games.py
@@ -1,6 +1,6 @@
-from mlb.retrosheet.parse import parse_files
+from mlb.retrosheet.chadwick import Events
 from mlb.util import commandline_args
 
 def run():
     args, extra_args = commandline_args('Parse Retrosheet extended game description')
-    parse_files(extra_args)
+    Events(extra_args)
