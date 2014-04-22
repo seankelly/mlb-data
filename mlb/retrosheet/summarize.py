b1f9f5ea90a1690e1e0df4c3ca428db290cda488
Sean Kelly <code@katron.org>
2014-04-21 20:20:35 -0500

    Separate stats for players by the season

--- mlb/retrosheet/summarize.py
+++ mlb/retrosheet/summarize.py
@@ -19,11 +19,15 @@ class SeasonSummary():
         # dict where the keys are the position and each position has keys for
         # the stats accumulated while the player was at that position.
         self.players = defaultdict(
-            lambda: {
-                'offense': defaultdict(int),
-                'pitching': defaultdict(int),
-                'fielding': defaultdict(lambda: defaultdict(int)),
-            }
+            # First step maps year to stats.
+            lambda: defaultdict(
+                # Second step maps type of stats to the stats themselves.
+                lambda: {
+                    'offense': defaultdict(int),
+                    'pitching': defaultdict(int),
+                    'fielding': defaultdict(lambda: defaultdict(int)),
+                }
+            )
         )
         # Only the offense key is used.
         self.stats = {
@@ -64,10 +68,10 @@ class SeasonSummary():
         Process a game's worth of events.
         """
         year, gameid, game_info, events = game
-        self.summarize_game_info(game_info)
-        self.summarize_game_events(events)
+        self.summarize_game_info(year, game_info)
+        self.summarize_game_events(year, events)
 
-    def summarize_game_info(self, game_info):
+    def summarize_game_info(self, year, game_info):
         """
         Summarize information about one game from cwgame.
         """
@@ -77,26 +81,26 @@ class SeasonSummary():
             pos = game_info[idx+1]
             if pos == 1:
                 continue
-            players[game_info[idx]]['offense']['GS'] += 1
-            players[game_info[idx]]['fielding'][pos]['GS'] += 1
+            players[game_info[idx]][year]['offense']['GS'] += 1
+            players[game_info[idx]][year]['fielding'][pos]['GS'] += 1
         # Credit pitcher win, loss, and save.
-        players[game_info[42]]['pitching']['W'] += 1
-        players[game_info[43]]['pitching']['L'] += 1
-        players[game_info[44]]['pitching']['S'] += 1
+        players[game_info[42]][year]['pitching']['W'] += 1
+        players[game_info[43]][year]['pitching']['L'] += 1
+        players[game_info[44]][year]['pitching']['S'] += 1
         # Credit game starters and finishers.
-        players[game_info[10]]['pitching']['GS'] += 1
-        players[game_info[11]]['pitching']['GS'] += 1
+        players[game_info[10]][year]['pitching']['GS'] += 1
+        players[game_info[11]][year]['pitching']['GS'] += 1
         # If there's no finishing pitcher, then it's a complete game.
         for gf_idx, gs_idx, other_score in [[82, 10, 35], [83, 11, 34]]:
             if game_info[gf_idx]:
-                players[game_info[gf_idx]]['pitching']['GF'] += 1
+                players[game_info[gf_idx]][year]['pitching']['GF'] += 1
             else:
-                players[game_info[gs_idx]]['pitching']['CG'] += 1
+                players[game_info[gs_idx]][year]['pitching']['CG'] += 1
                 # Check if it's a shutout.
                 if game_info[other_score] == 0:
-                    players[game_info[gs_idx]]['pitching']['SHO'] += 1
+                    players[game_info[gs_idx]][year]['pitching']['SHO'] += 1
 
-    def summarize_game_events(self, events):
+    def summarize_game_events(self, year, events):
         # Keep track of all players that appear in this game.
         appeared = {
             'offense': set(),
@@ -106,13 +110,13 @@ class SeasonSummary():
         }
         for event in events:
             involved = self.players_involved(event)
-            self._summarize_event_batter(event, appeared, involved)
-            self._summarize_event_baserunning(event, involved)
-            self._summarize_event_pitching(event, appeared, involved)
-            self._summarize_event_defense(event, involved)
-        self._summarize_game_appearances(appeared)
+            self._summarize_event_batter(year, event, appeared, involved)
+            self._summarize_event_baserunning(year, event, involved)
+            self._summarize_event_pitching(year, event, appeared, involved)
+            self._summarize_event_defense(year, event, involved)
+        self._summarize_game_appearances(year, appeared)
 
-    def _summarize_event_batter(self, event, appeared, involved):
+    def _summarize_event_batter(self, year, event, appeared, involved):
         players = self.players
         batter = involved['batter']
         # Batter accounting.
@@ -124,26 +128,26 @@ class SeasonSummary():
             appeared['fielding'][event[32]].add(involved['batter'])
         stat = self.event_types[event[34]]
         if stat and stat in self.stats['offense']:
-            players[batter]['offense'][stat] += 1
+            players[batter][year]['offense'][stat] += 1
         # Field 36 indicates whether the event counts as an official at
         # bat. Use that instead of trying to calculate it. The one
         # downside is if trying to apply historical rules since Chadwick
         # uses modern rules.
         if event[36]:
-            players[batter]['offense']['PA'] += 1
-            players[batter]['offense']['AB'] += 1
+            players[batter][year]['offense']['PA'] += 1
+            players[batter][year]['offense']['AB'] += 1
         elif event[38]:
-            players[batter]['offense']['PA'] += 1
-            players[batter]['offense']['SH'] += 1
+            players[batter][year]['offense']['PA'] += 1
+            players[batter][year]['offense']['SH'] += 1
         elif event[39]:
-            players[batter]['offense']['PA'] += 1
-            players[batter]['offense']['SF'] += 1
+            players[batter][year]['offense']['PA'] += 1
+            players[batter][year]['offense']['SF'] += 1
         elif 14 <= event[34] <= 16:
-            players[batter]['offense']['PA'] += 1
+            players[batter][year]['offense']['PA'] += 1
         # Field 43 is the RBI on play.
-        players[involved['batter']]['offense']['RBI'] += event[43]
+        players[involved['batter']][year]['offense']['RBI'] += event[43]
 
-    def _summarize_event_baserunning(self, event, involved):
+    def _summarize_event_baserunning(self, year, event, involved):
         players = self.players
         # Base running accounting. The batter is included when calculating
         # scoring a run.
@@ -151,15 +155,15 @@ class SeasonSummary():
         for idx in [58, 59, 60, 61]:
             if event[idx] >= 4:
                 base = 'base' + str(idx-58)
-                players[involved[base]]['offense']['R'] += 1
+                players[involved[base]][year]['offense']['R'] += 1
                 if idx > 58:
                     charged_pitcher = event[75+idx-59]
                 else:
                     charged_pitcher = involved[1]
-                players[charged_pitcher]['pitching']['R'] += 1
+                players[charged_pitcher][year]['pitching']['R'] += 1
                 # Code of 5 means the run is unearned.
                 if event[idx] != 5:
-                    players[charged_pitcher]['pitching']['ER'] += 1
+                    players[charged_pitcher][year]['pitching']['ER'] += 1
 
         # Count up base running stats.
         baserunning = [
@@ -172,66 +176,66 @@ class SeasonSummary():
             for idx in offsets:
                 if event[idx]:
                     base = 'base' + str(idx-offset+1)
-                    players[involved[base]]['offense'][br_stat] += 1
+                    players[involved[base]][year]['offense'][br_stat] += 1
 
-    def _summarize_event_pitching(self, event, appeared, involved):
+    def _summarize_event_pitching(self, year, event, appeared, involved):
         players = self.players
         # These are the indices in event_types that apply to pitchers.
         pitcher_stats = set([3, 9, 11, 14, 15, 16, 20, 21, 22, 23])
         # Pitcher accounting.
         if event[34] in pitcher_stats:
             stat = self.event_types[event[34]]
-            players[involved[1]]['pitching'][stat] += 1
+            players[involved[1]][year]['pitching'][stat] += 1
         # Field 40 is number of outs in the event. None of the other stats
         # affect the 'O' field for the pitcher.
         appeared['pitching'].add(involved[1])
-        players[involved[1]]['pitching']['O'] += event[40]
+        players[involved[1]][year]['pitching']['O'] += event[40]
         for pos in xrange(2, 10):
             p = involved[pos]
             appeared['fielding'][pos].add(p)
-            players[p]['fielding'][pos]['O'] += event[40]
+            players[p][year]['fielding'][pos]['O'] += event[40]
 
-    def _summarize_event_defense(self, event, involved):
+    def _summarize_event_defense(self, year, event, involved):
         players = self.players
         # Defense accounting.
         # Track WP and PB for catchers.
         if event[44]:
-            players[involved[2]]['fielding'][2]['WP'] += 1
+            players[involved[2]][year]['fielding'][2]['WP'] += 1
         if event[45]:
-            players[involved[2]]['fielding'][2]['PB'] += 1
+            players[involved[2]][year]['fielding'][2]['PB'] += 1
         if event[52] != 0:
-            players[involved[event[52]]]['fielding'][event[52]]['E'] += 1
+            players[involved[event[52]]][year]['fielding'][event[52]]['E'] += 1
         if event[54] != 0:
-            players[involved[event[54]]]['fielding'][event[54]]['E'] += 1
+            players[involved[event[54]]][year]['fielding'][event[54]]['E'] += 1
         if event[56] != 0:
-            players[involved[event[56]]]['fielding'][event[56]]['E'] += 1
+            players[involved[event[56]]][year]['fielding'][event[56]]['E'] += 1
         # Record who got the putouts.
         for idx in xrange(88, 91):
             if event[idx] != 0:
                 pos = event[idx]
                 player = involved[pos]
-                players[player]['fielding'][pos]['PO'] += 1
+                players[player][year]['fielding'][pos]['PO'] += 1
                 # Fields 41 and 42 are the double play and triple play
                 # turned flags.
                 if event[41] or event[42]:
-                    players[player]['fielding'][pos]['DP'] += 1
+                    players[player][year]['fielding'][pos]['DP'] += 1
         # Record all of the assists, if any.
         for idx in xrange(91, 96):
             if event[idx] != 0:
                 pos = event[idx]
-                players[involved[pos]]['fielding'][pos]['A'] += 1
+                players[involved[pos]][year]['fielding'][pos]['A'] += 1
 
-    def _summarize_game_appearances(self, appeared):
+    def _summarize_game_appearances(self, year, appeared):
         players = self.players
         # Credit each player with a game played at the appropriate position(s).
         # Iterate in a specific order.
         for idx, what in enumerate(['offense', 'pitching']):
             for player in appeared[what]:
-                players[player][what]['G'] += 1
+                players[player][year][what]['G'] += 1
         # Fielding requires indexing position, so put in a different loop.
         for pos in xrange(1, 11):
             for player in appeared['fielding'][pos]:
-                players[player]['fielding'][pos]['G'] += 1
+                players[player][year]['fielding'][pos]['G'] += 1
 
     def players_involved(self, event):
         players = {
